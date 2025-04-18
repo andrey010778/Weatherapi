3a7eaf8eff351d4021978de24a5599be3a78f97d
Андрей <7kuz@mail.ru>
2025-04-18 20:26:23 +0300

    Add gunicorn and numpy, change app.py to deploy

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.LUX])
 app.title = "Состояние воздуха"
+server = app.server #  добавляем для деплоя
 
 app.layout = create_layout()
 
 register_callbacks(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False) # При продакшене используем False вместо True
